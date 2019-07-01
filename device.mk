7b54eabde5700c1bae9a9ef0b21c6d5157440356
TheScarastic <warabhishek@gmail.com>
2019-07-02 01:32:47 +0200

    lmi: Initial fod impl
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: PIPIPIG233666 <pig.priv@gmail.com>
    Co-authored-by: TH779 <tongsj2333@gmail.com>
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: If756951a946c78a419d87522b71a44c3724a96da

--- device.mk
+++ device.mk
@@ -28,6 +28,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/xiaomi \
     vendor/nxp/opensource/sn100x
 
 # Screen density
@@ -92,7 +93,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
@@ -203,6 +205,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_lmi
 
 # Fastbootd
 PRODUCT_PACKAGES += \
