cb80f17dfc37f8b2e2fe02ae45237608d9c4864d
Nicholas Ambur <nambur@google.com>
2020-03-02 15:05:49 -0800

    lmi: Add SoundTrigger HAL v2.3 support
    Bug: 139071862
    Test: gts-tradefed run gts-dev -m GtsAssistIntentTestCases
    Change-Id: I2d17aade762ceb7800d5305119c37ec0f5991f35

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
