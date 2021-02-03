59e27d8e4ccf69da6d9339e074d593ca5c194d2d
TH779 <tongsj2333@gmail.com>
2021-02-03 22:13:49 +0800

    lmi: Import custom lights HAL from sm6150-common
     * From https://github.com/LineageOS/android_device_xiaomi_sm6150-common/commit/a674452353091ca6191fc505c9c3a3cd46e090ad.
     * In addition, this HAL has been cleaned up and adapted to the SM8250 platform.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Ia4f02abe8e27f7ca867272c765607e5bd35854d4

--- device.mk
+++ device.mk
@@ -255,6 +255,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_lmi
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
