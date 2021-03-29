472c04582232e7ffdb3bdfcdb9666086bf749954
TH779 <tongsj2333@gmail.com>
2021-03-29 18:08:37 +0800

    lmi: Build new aidl QTI vibrator HAL
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: I7a68b43ca2533e01073028fa0c0919222f5aa584

--- device.mk
+++ device.mk
@@ -422,7 +422,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator@1.2-service
+    vendor.qti.hardware.vibrator.service
 
 # WiFi
 PRODUCT_PACKAGES += \
