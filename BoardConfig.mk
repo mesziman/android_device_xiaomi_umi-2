c3c17fe868e9907486dc739af73eab38baaa849b
Isaac Chen <tingyi364@gmail.com>
2021-05-07 22:51:50 +0800

    lmi: Initial sepolicy
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: TH779 <tongsj2333@gmail.com>
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: I461cd23fc25bc5183f8425257e178626ab8651f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,10 @@ VENDOR_SECURITY_PATCH := 2021-04-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Treble
 BOARD_VNDK_VERSION := current
 
