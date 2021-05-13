29a85d5a686fccf96eff9cddc5eec8f2571b70f7
TH779 <tongsj2333@gmail.com>
2021-05-13 20:08:49 +0800

    lmi: Update build fingerprint from V12.2.4.0.RJKCNXM
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: I5192a4beed4e43fe189559e6e0e3c9e3592b1302

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -25,4 +25,4 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.2.3.0.RJKCNXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.2.4.0.RJKCNXM:user/release-keys
