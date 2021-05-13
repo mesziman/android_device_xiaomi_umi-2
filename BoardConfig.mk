1faa38e1fd0cf46b4c81ae861961d3dddc32dcf8
Mesquita <mesquita@aospa.co>
2021-05-12 21:43:03 -0300

    umi: Do not explicitly include qcom sepolicy
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,8 +155,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
-
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
