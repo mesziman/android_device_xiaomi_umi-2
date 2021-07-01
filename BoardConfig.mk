a517cc85a329511d584f193b449266f932ed391e
LuK1337 <priv.luk@gmail.com>
2021-07-01 17:33:22 +0800

    umi: Unset BOARD_VNDK_VERSION
    Change-Id: Ie9e8615eb7fe186dbb595fc3fa0d2bfc013b5d47
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,9 +174,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-# Treble
-BOARD_VNDK_VERSION := current
-
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
