19dd2181f299baa90543976f86c75e4f2e97cbac
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-15 21:01:02 +0800

    umi: vintf: Add a minimal framework compatibility matrix
    * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Change-Id: I180d19e7939ee0c3e9f1aeb04a854f8aaf444235
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 
