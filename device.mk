0061decdc358bd9fd608269498649269f5dee323
chaptsand <chaptsand@gmail.com>
2021-08-09 14:09:16 +0800

    umi: Drop lineage livedisplay hal
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- device.mk
+++ device.mk
@@ -262,10 +262,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_umi
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
