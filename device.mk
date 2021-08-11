d1853ae1af654ab1f62ba83191c324559c6bdd53
LuK1337 <priv.luk@gmail.com>
2021-08-11 22:52:35 +0800

    umi: Remove fs_config_files from PRODUCT_PACKAGES
    * Not needed anymore.
    
    Change-Id: Iaa474730fe5fdb5d5e159af127613fa4a22fc99e

--- device.mk
+++ device.mk
@@ -62,10 +62,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# AID/fs configs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong
